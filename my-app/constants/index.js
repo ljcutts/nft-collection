99588b8717c280fa582a2f4a431bf744580b2c67
Larry <larry@My-Mac.local>
2022-02-08 10:51:27 -0600

    fix contract address

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -581,4 +581,4 @@ export const abi = [
     type: "receive",
   },
 ];
-export const NFT_CONTRACT_ADDRESS = " 0xF118C8F4bfA0df3AAa5CDCA83D16b9E70580dD87";
+export const NFT_CONTRACT_ADDRESS = "0xF118C8F4bfA0df3AAa5CDCA83D16b9E70580dD87";
